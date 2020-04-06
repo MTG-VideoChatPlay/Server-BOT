a121bfd02c12dcc47ac507ed2cd1bb6ba6a47efb
Hazy <cktheking11@gmail.com>
2020-04-05 22:40:12 -0500

    first success

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 
-token = "Njk2NTUyMDE1MjU3NjY1NTQ3.XoqfQw.zrnEpwJQ56ScZ9Vi1NdvfKgzKSg"
+token = ""
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
