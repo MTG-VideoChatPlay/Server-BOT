a928c0b945bcd7f677dd61ca9ba6c12d01b03081
Hazy <cktheking11@gmail.com>
2020-04-08 18:17:37 -0500

    v0.1.4

--- serverbot.py
+++ serverbot.py
@@ -6,15 +6,12 @@ import operator
 import json
 from collections import Counter
 
-token = "Njk2NTUyMDE1MjU3NjY1NTQ3.XozjZQ.98EpSLsSfmIyzeQ22jrre_eVulY"
+token = "Njk2NTUyMDE1MjU3NjY1NTQ3.Xo0k8w.yhi_3g-cQfnO769PLW_ZP8Gta_Y"
 bot = commands.Bot(command_prefix='*', activity=discord.Game(name="Prefix is *"))
 
 bot.player = []
-bot.players_pod1 = []
 bot.players_pod1_score = [0, 0, 0, 0, 0, 0, 0, 0]
-bot.players_pod2 = []
 bot.players_pod2_score = [0, 0, 0, 0, 0, 0, 0, 0]
-bot.players_pod3 = []
 bot.players_pod3_score = [0, 0, 0, 0, 0, 0, 0, 0]
 bot.playersdictpod1 = {}
 bot.playersdictpod2 = {}
@@ -33,8 +30,9 @@ async def on_ready():
 @bot.command()
 async def register(ctx, *person):
     bot.player = list(person)
+    await ctx.send("Registered " + str(len(person)) + " players!")
 
-
+#WORKS PERFECTLY
 @bot.command()
 async def generatepods(ctx, numberofpods):
     random.shuffle(bot.player)
@@ -42,51 +40,38 @@ async def generatepods(ctx, numberofpods):
     a = 0
     for i in bot.player:
         if a < x:
-            bot.players_pod1.append(bot.player[a])
+            bot.playersdictpod1.update({bot.player[a]:0})
         if x <= a < (2*x):
-            bot.players_pod2.append(bot.player[a])
+            bot.playersdictpod2.update({bot.player[a]:0})
         if (2*x) <= a < (3*x):
-            bot.players_pod3.append(bot.player[a])
+            bot.playersdictpod3.update({bot.player[a]:0})
         a += 1
-    await ctx.send("Registered: \nPod 1: " + str(bot.players_pod1) + "\nPod 2: " + str(bot.players_pod2) + "\nPod 3: " + str(bot.players_pod3))
+    await ctx.send("Registered: \nPod 1: " + str(bot.playersdictpod1) + "\nPod 2: " + str(bot.playersdictpod2) + "\nPod 3: " + str(bot.playersdictpod3))
 
 
 @bot.command()
 async def generatetables(ctx, pod, round=1):
     if int(pod) == 1:
-        if int(round) == 1:
-            random.shuffle(bot.players_pod1)
-        else:
-            bot.players_pod1 = list(bot.playersdictpod1)
-        table1 = bot.players_pod1[0] + " " + bot.players_pod1[1]
-        table2 = bot.players_pod1[2] + " " + bot.players_pod1[3]
-        table3 = bot.players_pod1[4] + " " + bot.players_pod1[5]
-        table4 = bot.players_pod1[6] + " " + bot.players_pod1[7]
+        table1 = list(bot.playersdictpod1.keys())[0] + " - " + list(bot.playersdictpod1.keys())[1]
+        table2 = list(bot.playersdictpod1.keys())[2] + " - " + list(bot.playersdictpod1.keys())[3]
+        table3 = list(bot.playersdictpod1.keys())[4] + " - " + list(bot.playersdictpod1.keys())[5]
+        table4 = list(bot.playersdictpod1.keys())[6] + " - " + list(bot.playersdictpod1.keys())[7]
         await ctx.send("Your tables for pod 1 are:\n" + table1 + "\n" + table2 + "\n" + table3 + "\n" + table4 + "\n")
     if int(pod) == 2:
-        if int(round) == 1:
-            random.shuffle(bot.players_pod1)
-        else:
-            bot.players_pod2 = list(bot.playersdictpod2)
-        random.shuffle(bot.players_pod2)
-        table5 = bot.players_pod2[0] + " " + bot.players_pod2[1]
-        table6 = bot.players_pod2[2] + " " + bot.players_pod2[3]
-        table7 = bot.players_pod2[4] + " " + bot.players_pod2[5]
-        table8 = bot.players_pod2[6] + " " + bot.players_pod2[7]
+        table5 = list(bot.playersdictpod2.keys())[0] + " " + list(bot.playersdictpod1.keys())[1]
+        table6 = list(bot.playersdictpod2.keys())[2] + " " + list(bot.playersdictpod1.keys())[3]
+        table7 = list(bot.playersdictpod2.keys())[4] + " " + list(bot.playersdictpod1.keys())[5]
+        table8 = list(bot.playersdictpod2.keys())[6] + " " + list(bot.playersdictpod1.keys())[7]
         await ctx.send("Your tables for pod 2 are:\n " + table5 + "\n" + table6 + "\n" + table7 + "\n" + table8 + "\n")
     if int(pod) == 3:
-        if int(round) == 1:
-            random.shuffle(bot.players_pod3)
-        else:
-            bot.players_pod3 = list(bot.playersdictpod3)
-        random.shuffle(bot.players_pod3)
-        table9 = bot.players_pod3[0] + " " + bot.players_pod3[1]
-        table10 = bot.players_pod3[2] + " " + bot.players_pod3[3]
-        table11 = bot.players_pod3[4] + " " + bot.players_pod3[5]
-        table12 = bot.players_pod3[6] + " " + bot.players_pod3[7]
+        table9 = list(bot.playersdictpod3.keys())[0] + " " + list(bot.playersdictpod3.keys())[1]
+        table10 = list(bot.playersdictpod3.keys())[2] + " " + list(bot.playersdictpod3.keys())[3]
+        table11 = list(bot.playersdictpod3.keys())[4] + " " + list(bot.playersdictpod3.keys())[5]
+        table12 = list(bot.playersdictpod3.keys())[6] + " " + list(bot.playersdictpod3.keys())[7]
         await ctx.send("Your tables for pod 3 are:\n" + table9 + "\n" + table10 + "\n" + table11 + "\n" + table12 + "\n")
 
 
+# fix ties
 @bot.command()
 async def gameover(ctx, pod, table, score):
     score.split("-")
@@ -119,20 +104,20 @@ async def gameover(ctx, pod, table, score):
 @bot.command()
 async def results(ctx, pod):
     a = 0
-    if pod == 1:
-        for player in bot.players_pod1:
-            bot.playersdictpod1[player] = bot.players_pod1_score[a]
+    if int(pod) == 1:
+        for player in bot.playersdictpod1:
+            bot.playersdictpod1[player] = bot.players_pod1_score[a] + bot.playersdictpod1[player]
             a += 1
         bot.sorted_playerdictpod1 = dict(sorted(bot.playersdictpod1.items(), key=operator.itemgetter(1), reverse=True))
         await ctx.send(bot.sorted_playerdictpod1)
-    if pod == 2:
-        for player in bot.players_pod2:
+    if int(pod) == 2:
+        for player in bot.playersdictpod2:
             bot.playersdictpod2[player] = bot.players_pod2_score[a]
             a += 1
         bot.sorted_playerdictpod2 = dict(sorted(bot.playersdictpod2.items(), key=operator.itemgetter(1), reverse=True))
         await ctx.send(bot.sorted_playerdictpod2)
-    if pod == 3:
-        for player in bot.players_pod3:
+    if int(pod) == 3:
+        for player in bot.playersdictpod3:
             bot.playersdictpod3[player] = bot.players_pod3_score[a]
             a += 1
         bot.sorted_playerdictpod3 = dict(sorted(bot.playersdictpod3.items(), key=operator.itemgetter(1), reverse=True))
@@ -142,9 +127,16 @@ async def results(ctx, pod):
 @bot.command()
 async def endtourney(ctx):
     bot.player = []
-    bot.players_pod1 = []
-    bot.players_pod2 = []
-    bot.players_pod3 = []
+    bot.players_pod1_score = [0, 0, 0, 0, 0, 0, 0, 0]
+    bot.players_pod2_score = [0, 0, 0, 0, 0, 0, 0, 0]
+    bot.players_pod3_score = [0, 0, 0, 0, 0, 0, 0, 0]
+    bot.playersdictpod1 = {}
+    bot.playersdictpod2 = {}
+    bot.playersdictpod3 = {}
+    bot.leaderboardpod1 = {}
+    bot.leaderboardpod2 = {}
+    bot.leaderboardpod3 = {}
+    bot.leaderboard = {}
     await ctx.send("Success!")
 
 
@@ -173,7 +165,7 @@ async def updateleaderboard(ctx, pod):
         A = Counter(oldleaderboard)
         B = Counter(bot.leaderboardpod1)
         C = Counter(bot.leaderboardpod2)
-        d = Counter(bot.leaderboardpod3)
+        D = Counter(bot.leaderboardpod3)
         newleaderboard = A + B + C + D
         leaderboarddisplay = "Leaderboard For Event\n-----------------------------\n"
         for key in newleaderboard:
